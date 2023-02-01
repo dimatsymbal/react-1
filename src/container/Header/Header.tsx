06aa1992f26393c01b3c61ef88ca71f839bb1f21
Dima <dima.tsymbal@outlook.com>
2023-02-01 15:18:26 +0200

    add logo

--- src/container/Header/Header.tsx
+++ src/container/Header/Header.tsx
@@ -1,11 +1,11 @@
 import AppBar from '@mui/material/AppBar'
 import Toolbar from '@mui/material/Toolbar'
-import Typography from '@mui/material/Typography'
 import Button from '@mui/material/Button'
 import IconButton from '@mui/material/IconButton'
 import MenuIcon from '@mui/icons-material/Menu'
 import { Container } from '@mui/material'
 import './header.scss'
+import Logo from 'Components/Logo/Logo'
 
 type Props = {}
 const Header = (props: Props) => {
@@ -22,13 +22,8 @@ const Header = (props: Props) => {
                     >
                         <MenuIcon />
                     </IconButton>
-                    <Typography
-                        variant="h6"
-                        component="div"
-                        sx={{ flexGrow: 1 }}
-                    >
-                        News
-                    </Typography>
+
+                    <Logo />
                     <Button color="inherit">Login</Button>
                 </Toolbar>
             </Container>
